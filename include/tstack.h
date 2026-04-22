13062a8bef042ae5a9c9eceef7aa45b49cd4dab0
Molman0 <evgenytyukalkin@yandex.com>
2026-04-22 13:37:05 +0300

    tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,43 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#define SIZE 100
 
-template<typename T, int size>
-class TStack {
-  // добавьте код стека
+template <typename T> class TStack {
+ private:
+  T stack[SIZE];
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+
+  int push(T value) {
+    if (topIndex >= SIZE - 1) {
+      throw std::runtime_error("Stack overflow!");
+    }
+    stack[++topIndex] = value;
+    return 0;
+  }
+
+  T pop() {
+    if (isEmpty()) {
+      throw std::runtime_error("Stack is empty!!");
+    }
+    return stack[topIndex--];
+  }
+
+  T top() const {
+    if (isEmpty()) {
+      throw std::runtime_error("Stack is empty!");
+    }
+    return stack[topIndex];
+  }
+
+  bool isEmpty() const { return topIndex == -1; }
+
+  bool isFull() const { return topIndex == SIZE - 1; }
+
+  int getSize() const { return topIndex + 1; }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif // INCLUDE_TSTACK_H_
